631e3678a6379ccfc0c78f35d13d7407c5d65dd7
Nouman Mullaji <107634861+03nouman@users.noreply.github.com>
2025-01-06 21:34:05 +0530

    Add files via upload

---
+++ 02_basics/02_Arrays.js
@@ -0,0 +1,38 @@
+const marvel_heroes = ["thor","ironman","spiderman"];
+
+const dc_heroes = ["superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes);
+
+//concat
+// let concatinatingArrays = marvel_heroes.concat(dc_heroes);
+// console.log(concatinatingArrays);
+
+// spread operators.
+let allHeroes = [...marvel_heroes,...dc_heroes]
+// console.log(allHeroes);
+
+const another_Arrays = [1,2,3,[4,5],6,[7,[6,5]]];
+
+let useAble_Array = another_Arrays.flat(Infinity); // Interesting
+// console.log(useAble_Array);
+
+console.log(Array.isArray("nouman"));
+
+console.log(Array.from("nouman"));
+
+console.log(Array.from({name:"nouman"}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
+
